b4622d27e9d017463bc3cf5faace039419138647
khouloud2407 <Khouloud.Laajili@esprit.tn>
2024-10-17 10:17:55 +0200

    FCBK

--- src/config/facebook.js
+++ src/config/facebook.js
@@ -29,31 +29,39 @@ app.use(session({
 passport.use(new FacebookStrategy({
     clientID: '1247083726303005',
     clientSecret: 'bbc0ecb060057a0c2edd62564b2769dd',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: 'http://localhost:3001/auth/facebook/callback',
     profileFields: ['id', 'displayName', 'emails']
   },
   async (accessToken, refreshToken, profile, done) => {
    // const connection = await mysql.createConnection(dbConfig);
     const rows = await db.execute('SELECT * FROM users WHERE facebookId = ?', [profile.id]);
      // Log values for debugging
-     console.log('Facebook ID:', profile.id);
-     console.log('Email:', profile.emails , 
-     );
-    if (rows.length > 0) {
-      done(null, rows[0]);
-    } else {
-        const result = await db.execute('INSERT INTO users (facebookId, name, email) VALUES (?, ?, ?)', [
-            profile.id,
-        profile.displayName,
-        profile.emails || '', 
-        ]);
-        console.log(result); // Log the result to see its structure
+     const email = profile.emails && profile.emails.length > 0 ? profile.emails[0].value : null;
+     if (!email) {
+      return done('Email not provided. Please enter your email address.'); // Pass the error to done
+  }
+   //  console.log('Facebook ID:', profile.id);
+   // console.log('Email:', profile.emails , 
+    // );
+  //  if (rows.length > 0) {
+   //   done(null, rows[0]);
+   // } else {
+     //   const result = await db.execute('INSERT INTO users (facebookId, name, email) VALUES (?, ?, ?)', [
+     //       profile.id,
+      //  profile.displayName,
+      //  profile.emails || generateTemporaryEmail(profile.displayName), 
+      //  ]);
+      //  console.log(result); // Log the result to see its structure
      
-      const newUser = { id: result.insertId, facebookId: profile.id, name: profile.displayName, email: profile.emails|| ''};
-      done('', newUser);
-    }
+      //const newUser = { id: result.insertId, facebookId: profile.id, name: profile.displayName, email: profile.emails||  profile.emails || generateTemporaryEmail(profile.displayName)};
+      //done('', newUser);
+  //  }
   }
 ));
+function generateTemporaryEmail(name) {
+  const sanitizedName = name.replace(/\s+/g, '').toLowerCase(); // Remove spaces and convert to lowercase
+  return `${sanitizedName}@WICd.com`; // Use your domain here
+}
 
 // Sérialiser et désérialiser l'utilisateur
 passport.serializeUser((user, done) => {
